6f760c20854973c9c893584895bcd6ea2db55960
LAU TRUONG MINH <163316784+LiuChangMing88@users.noreply.github.com>
2024-09-26 10:51:33 +0700

    test

---
+++ ngu.py
@@ -0,0 +1,7 @@
+# Wrong:
+# operators sit far away from their operands
+income = (gross_wages +
+          taxable_interest +
+          (dividends - qualified_dividends) -
+          ira_deduction -
+          student_loan_interest)
